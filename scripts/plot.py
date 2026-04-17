34652db20fcbd28c15b065705d86187256a8c441
Pietro Businaro <pietro.businaro@mail.polimi.it>
2026-04-17 16:10:07 +0200

    Update plot.py with new .csv files and add particles on plot

--- scripts/plot.py
+++ scripts/plot.py
@@ -1,18 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = np.loadtxt("velocity.csv", delimiter=",")
-# Points
-px = data[:, 0]
-py = data[:, 1]
-pz = data[:, 2]
-# Velocity
-vx = data[:, 3]
-vy = data[:, 4]
-vz = data[:, 5]
+mesh = np.loadtxt("mesh.csv", delimiter=",")
+mesh_x = mesh[:, 0]
+mesh_y = mesh[:, 1]
+
+flow_field = np.loadtxt("flow_field.csv", delimiter=",")
+flow_field_x = flow_field[:, 0]
+flow_field_y = flow_field[:, 1]
+
+particles = np.loadtxt("particles_000000.csv", delimiter=",")
+particles_x = particles[:, 0]
+particles_y = particles[:, 1]
 
 plt.figure()
-plt.quiver(px, py, vx, vy, np.hypot(vx, vy), cmap='jet')
+plt.quiver(mesh_x,
+           mesh_y,
+           flow_field_x,
+           flow_field_y,
+           np.hypot(flow_field_x, flow_field_y),
+           cmap='jet')
+plt.scatter(particles_x, particles_y, c="black")
 plt.colorbar()
 plt.tight_layout()
 plt.show()
