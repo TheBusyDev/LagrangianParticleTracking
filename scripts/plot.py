03aeaeb8f26a5941f30656eef05423482905dd4f
Pietro Businaro <pietro.businaro@mail.polimi.it>
2026-04-20 15:57:43 +0200

    Create animation in plot.py

--- scripts/plot.py
+++ scripts/plot.py
@@ -1,26 +1,68 @@
+import glob
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
-mesh = np.loadtxt("mesh.csv", delimiter=",")
-mesh_x = mesh[:, 0]
-mesh_y = mesh[:, 1]
 
-flow_field = np.loadtxt("flow_field.csv", delimiter=",")
-flow_field_x = flow_field[:, 0]
-flow_field_y = flow_field[:, 1]
+def load_timesteps(basename: str) -> list[np.ndarray]:
+  '''
+  Load data from basename_XXXXXX.csv files.
+  Return a list of numpy arrays, one for each timestep.
+  '''
+  filenames = glob.glob(f"{basename}_*.csv")
+  result = list()
 
-particles = np.loadtxt("particles_000000.csv", delimiter=",")
-particles_x = particles[:, 0]
-particles_y = particles[:, 1]
+  for filename in filenames:
+    timestep = filename.removeprefix(f"{basename}_").removesuffix(".csv")
+    timestep = int(timestep)
+    data = np.loadtxt(filename, delimiter=",")
+    result.append(data)
 
-plt.figure()
-plt.quiver(mesh_x,
-           mesh_y,
-           flow_field_x,
-           flow_field_y,
-           np.hypot(flow_field_x, flow_field_y),
-           cmap='jet')
-plt.scatter(particles_x, particles_y, c="black")
-plt.colorbar()
-plt.tight_layout()
-plt.show()
+  return result
+
+
+# MAIN PROGRAM.
+if __name__ == "__main__":
+  # Load mesh.
+  mesh = np.loadtxt("mesh.csv", delimiter=",")
+
+  # Load flow field and particles.
+  flow_fields = load_timesteps("flow_field")
+  particles = load_timesteps("particles")
+
+  n_timesteps = len(flow_fields)
+  assert len(particles) == n_timesteps
+
+  # Plot.
+  fig = plt.gcf()
+  ax = plt.gca()
+
+  # Initialize plots.
+  quiver_plot = ax.quiver(mesh[:, 0],
+                          mesh[:, 1],
+                          flow_fields[0][:, 0],
+                          flow_fields[0][:, 1],
+                          np.linalg.norm(flow_fields[0], axis=1),
+                          cmap="jet")
+  fig.colorbar(quiver_plot)
+
+  scatter_plot = ax.scatter(particles[0][:, 0],
+                              particles[0][:, 1],
+                              c="black")
+
+  ax.set_aspect('equal')
+  fig.tight_layout()
+
+  # Plot one single frame.
+  def plot_frame(frame: int) -> None:
+    i = frame % n_timesteps
+
+    quiver_plot.set_UVC(flow_fields[i][:, 0],
+                        flow_fields[i][:, 1],
+                        np.linalg.norm(flow_fields[i], axis=1))
+
+    scatter_plot.set_offsets(particles[i][:, :2])
+
+  animation = FuncAnimation(fig, plot_frame)
+  plt.show()
+  exit(0)
